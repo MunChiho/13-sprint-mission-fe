13a74111c42a516631b68e320144ec67088a3850
sgwag6799 <sgwag6799@gmail.com>
2026-04-17 13:18:34 +0900

    feat: ArticleService.js ProductService.s

---
+++ main.js
@@ -0,0 +1,19 @@
+// [ ]  이외의 코드들은 모두 main.js 파일에 작성해 주세요.
+// [ ] import를 활용해 주세요.
+// [ ] 각 함수를 실행하는 코드를 작성하고, 제대로 동작하는지 확인해 주세요.
+import {
+  getArticleList,
+  getArticle,
+  createArticle,
+  patchArticle,
+  deleteArticle,
+} from "./ArticleService.js";
+import {
+  getProductList,
+  getProduct,
+  createProduct,
+  patchProduct,
+  deleteProduct,
+} from "./ProductService.js";
+
+console.log("main.js 연결 완료!");
