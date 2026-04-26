12480b780a30e08dadd71c396cb9afac3a49d7c1
cbg01-cbg <chbgn1@gmail.com>
2026-04-26 20:37:07 +0900

    스프린트 미션3 제출

--- 실습/interactive-js-practice-main/script.js
+++
@@ -1,6 +0,0 @@
-// 이동은 append로!
-const element = document.querySelector("#element");
-const newParent = document.querySelector("#new-parent");
-
-// element가 newParent의 자식으로 이동
-newParent.append(element);
