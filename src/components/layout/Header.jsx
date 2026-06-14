78b4828ff6a91d52c8dd47112522d22af56a3a66
MunChiho <mch52373820@gmail.com>
2026-06-14 15:33:11 +0900

    feat: 기존 svg삭제 및 푸터 반응형 구현

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -7,26 +7,26 @@ import { usePathname } from "next/navigation";
 export default function Header() {
   const pathname = usePathname();
   return (
-    <header className="w-full h-17.5 py-2.5 px-4 tablet:px-6 desktop:py-2.25 desktop:px-50 flex items-center justify-between border-b border-gray-200">
+    <header className="w-full h-17.5 py-2.5 px-4 md:px-6 lg:py-2.25 lg:px-50 flex items-center justify-between border-b border-gray-200">
       <div className="flex items-center">
         <Link className="flex items-center gap-[9.592px]" href="/">
           <Image
-            className="hidden tablet:block"
+            className="hidden md:block"
             src="/image/header.png"
             width={40}
             height={40}
             alt="판다마켓"
           />
-          <span className="text-[#3692FF] text-[20.202px] tablet:text-[25.633px] font-bold pt-[6.305px] leading-normal pb-[6.695px]"
+          <span className="text-primary-100 text-[20.202px] md:text-[25.633px] font-bold pt-[6.305px] leading-normal pb-[6.695px]"
             style={{fontFamily: "ROKAF Sans"}}>
             판다마켓
           </span>
         </Link>
-        <nav className="flex ml-4 tablet:ml-5 desktop:ml-8 gap-2 text-base">
+        <nav className="flex ml-4 md:ml-5 lg:ml-8 gap-2 text-base">
           <Link
             href="/freeboard"
-            className={`flex justify-center items-center tablet:py-6 tablet:pr-4 tablet:pl-3.75 text-md tablet:text-2lg font-bold ${
-              pathname === "/freeboard" ? "text-[#3692FF]" : "text-[#4B5563]"
+            className={`flex justify-center items-center md:py-6 md:pr-4 md:pl-3.75 text-md md:text-2lg font-bold ${
+              pathname === "/freeboard" ? "text-primary-100" : "text-gray-600"
             }`}
           >
             자유게시판
@@ -34,8 +34,8 @@ export default function Header() {
 
           <Link
             href="/market"
-            className={`flex justify-center items-center tablet:py-6 tablet:px-5.75 text-md tablet:text-2lg font-bold ${
-              pathname === "/market" ? "text-[#3692FF]" : "text-[#4B5563]"
+            className={`flex justify-center items-center md:py-6 md:px-5.75 text-md md:text-2lg font-bold ${
+              pathname === "/market" ? "text-primary-100" : "text-gray-600"
             }`}
           >
             중고마켓
@@ -43,7 +43,7 @@ export default function Header() {
         </nav>
       </div>
       <Link href="/login">
-        <button className="inline-flex bg-[#3692FF] h-10.5 px-5.75 py-3 justify-center items-center gap-2.5 rounded-lg text-lg tablet:text-2lg font-semibold text-white">
+        <button className="inline-flex bg-primary-100 h-10.5 px-5.75 py-3 justify-center items-center gap-2.5 rounded-lg text-lg md:text-2lg font-semibold text-white">
           로그인
         </button>
       </Link>
